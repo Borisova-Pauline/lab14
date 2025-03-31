70785aa5fccd5eb69da150583f9ef79cf51f21e4
Pauline <some@mail>
2025-03-31 13:14:17 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         String addresses = "1.76.54  567.765.890.342  43.255.218.10  192.168.1.10  76.345.6  192.168.2.20";
-        Pattern p1 = Pattern.compile("((1?\\d?\\d|2[0-4]\\d|25[0-5])\\.){3}1?\\d?\\d|\\2");
+        Pattern p1 = Pattern.compile("((1?\\d?\\d|2[0-4]\\d|25[0-5])\\.){3}1?\\d?\\d");
         Pattern p2 = Pattern.compile("\\s+");
         Matcher m;
         String[] ids = p2.split(addresses);
